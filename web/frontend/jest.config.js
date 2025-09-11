4e1a680a11533a9cdb74a41f2e1bb273ece77cca
Getuar Kelmendi <getuarkelmendi6@gmail.com>
2025-09-11 17:40:54 +0200

    Fix dependency versions and Jest configuration for pipeline compatibility

--- web/frontend/jest.config.js
+++ web/frontend/jest.config.js
@@ -1,5 +1,8 @@
 export default {
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
